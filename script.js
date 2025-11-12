bdaa6c4db891aedc2665a092256fedea9a6c7758
Franklin Toro <fatoro@puce.edu.ec>
2025-11-11 19:53:59 -0500

    first : envio de formulario

--- script.js
+++ script.js
@@ -56,21 +56,34 @@ function fizzbazz(divisorFizz, divisorBazz, square, number) {
     }
 }
 
+function buttonClicked(){
+    console.log("me Hicieron clickcitooo")
 
+const dimensionInput = document.getElementById("dimension")
+const fizzInput = document.getElementById("fizz-divisor")
+const bazzInput = document.getElementById("bazz-divisor")
 
+const dimensionValue= parseInt(dimensionInput.value)
+const fizzValue= parseInt(fizzInput.value)
+const bazzValue= parseInt(bazzInput.value)
 
+console.log(`El valor de la dimension es:${dimensionValue}`)
+console.log(`El valor de fizz es:${fizzValue}`)
+console.log(`El valor de bazz es:${bazzValue}`)
+
+const matrixContainer = document.getElementById("matrix-container");
+createMatrix(dimensionValue, matrixContainer, fizzValue, bazzValue);
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Ya mismo aprendo con suficiente practica")
-    const container = document.querySelector(".container")
-    createMatrix(5,container,2,3) //(2*3))
 
-  
+    const btnGenerate = document.getElementById("Generate-button")
+    btnGenerate.addEventListener("click", buttonClicked)
 
 
-  
+ 
 
-})
+});
 
 
 
